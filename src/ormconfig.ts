6f9088b64dd4f6d34029eebec07cc88f75804322
SabeelNasir <33089105+SabeelNasir@users.noreply.github.com>
2020-05-27 06:18:21 +0500

    deploying

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -2,29 +2,10 @@ import "./lib/env"
 import path from 'path';
 const entityPath = path.resolve(__dirname + "/models/entities/*.js");
 console.log("entity path", entityPath);
+console.log(process.env.APP_DB_HOST)
+console.log(process.env.APP_DB_PASS)
+console.log(process.env.APP_DB_NAME)
 const OrmConfig: any = {
-    "type": "mysql",
-    "host": process.env.APP_DB_HOST || "localhost",
-    "port": Number(process.env.APP_DB_PORT) || 3306,
-    "username": process.env.APP_DB_USER || "root",
-    // "password": process.env.APP_DB_PASS || "",
-    "database": process.env.APP_DB_NAME || "stonefly_db",
-    "logging": false,
-    "entities": [
-        entityPath
-    ],
-    cache: {
-        type: "database",
-        tableName: "query-result-cache",
-        duration: 100000
-    },
-    pool: {
-        max: 100,
-        min: 10
-    }
-}
-
-const OrmConfigProd: any = {
     "type": "mysql",
     "host": process.env.APP_DB_HOST || "localhost",
     "port": Number(process.env.APP_DB_PORT) || 3306,
@@ -46,4 +27,4 @@ const OrmConfigProd: any = {
     }
 }
 
-export default process.env.NODE_ENV == 'development' ? OrmConfig : OrmConfigProd;
+export default OrmConfig;
